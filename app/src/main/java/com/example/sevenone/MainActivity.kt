ca78d38bbae741b6c7255b9a5b7b4c4e44085433
atipozitiv <117564743+atipozitiv@users.noreply.github.com>
2024-05-07 21:04:51 +0700

    <3

--- app/src/main/java/com/example/sevenone/MainActivity.kt
+++ app/src/main/java/com/example/sevenone/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.sevenone
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.CompoundButton
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectCollection
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.user_location.UserLocationLayer
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
+    private lateinit var myLoc: UserLocationLayer
+    private lateinit var obj: MapObjectCollection
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +34,26 @@ class MainActivity : AppCompatActivity() {
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapview)
+        getLoc()
 
-        var compassBut: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        val compassBut: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        val toggleBut: ToggleButton = findViewById(R.id.toggleButton)
+        val mapKit: MapKit = MapKitFactory.getInstance()
+        val trafficJam = mapKit.createTrafficLayer(mapView.mapWindow)
+        myLoc = mapKit.createUserLocationLayer(mapView.mapWindow)
+        obj = mapView.map.mapObjects
+
+        mapView.map.addInputListener(myList)
+
         compassBut.setOnClickListener {
             mapView.map.move(
                 CameraPosition(
                     Point(55.3549067168026,86.08633428239138),17.0f, 0.0f, 0.0f,
                 )
             )
+            myLoc.isVisible = true
         }
 
-        var toggleBut: ToggleButton = findViewById(R.id.toggleButton)
-        var mapKit: MapKit = MapKitFactory.getInstance()
-        var trafficJam = mapKit.createTrafficLayer(mapView.mapWindow)
         toggleBut.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 trafficJam.isTrafficVisible = true
@@ -55,5 +73,26 @@ class MainActivity : AppCompatActivity() {
         mapView.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
+    }
+
+    fun getLoc() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 0)
+        return
+    }
+
+    fun addObj(point: Point){
+        obj.addPlacemark(point)
+    }
+    val myList = object:InputListener {
+        override fun onMapTap(map: Map, point: Point) {
+            addObj(point)
+            Log.i("dsf","sdlfkj")
+        }
+
+        override fun onMapLongTap(p0: Map, p1: Point) {
+            Log.i("dsf","sdlfkj2")
+        }
     }
 }
